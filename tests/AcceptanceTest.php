7041f6a508ecfbf80c1ca56fcd4bce616faf9dde
Daniele Megna <daniele.megna@qmates.tech>
2024-08-10 10:01:27 +0200

    Refactor in acceptance tests

--- tests/AcceptanceTest.php
+++ tests/AcceptanceTest.php
@@ -3,21 +3,25 @@
 use PHPUnit\Framework\TestCase;
 
 class AcceptanceTest extends TestCase {
+    
+    /** @var RetailCalculator */
+    private $retailCalculator;
+
+    protected function setUp(): void {
+        $this->retailCalculator = new RetailCalculator();
+    }
 
     /** @test */
     public function singleOneHundredDollarsProductInUtah() {
-        $retailCalculator = new RetailCalculator();
-
-        $actual = $retailCalculator->totalFor([new PurchaseItem(100)], 'UT');
+        $items = [new PurchaseItem(100)];
+        $actual = $this->retailCalculator->totalFor($items, 'UT');
 
         $this->assertEquals(106.85, $actual);
     }
 
     /** @test */
     public function someProductsInUtah() {
-        $retailCalculator = new RetailCalculator();
-
-        $actual = $retailCalculator->totalFor([
+        $actual = $this->retailCalculator->totalFor([
             new PurchaseItem(240),
             new PurchaseItem(170),
             new PurchaseItem(90)
@@ -28,9 +32,7 @@ class AcceptanceTest extends TestCase {
 
     /** @test */
     public function roundPriceToCentsUp() {
-        $retailCalculator = new RetailCalculator();
-
-        $actual = $retailCalculator->totalFor([new PurchaseItem(110.80)], 'UT');
+        $actual = $this->retailCalculator->totalFor([new PurchaseItem(110.80)], 'UT');
 
         // 118.3898
         $this->assertEquals(118.39000, $actual);
@@ -38,9 +40,7 @@ class AcceptanceTest extends TestCase {
 
     /** @test */
     public function alwaysRoundPriceToCentsUpAlsoWhenUnderHalf() {
-        $retailCalculator = new RetailCalculator();
-
-        $actual = $retailCalculator->totalFor([new PurchaseItem(270.99)], 'UT');
+        $actual = $this->retailCalculator->totalFor([new PurchaseItem(270.99)], 'UT');
 
         // 289.552815
         $this->assertEquals(289.56000, $actual);
@@ -48,9 +48,7 @@ class AcceptanceTest extends TestCase {
 
     /** @test */
     public function someProductsInNevada() {
-        $retailCalculator = new RetailCalculator();
-
-        $actual = $retailCalculator->totalFor([
+        $actual = $this->retailCalculator->totalFor([
             new PurchaseItem(380),
             new PurchaseItem(250),
             new PurchaseItem(155)
@@ -61,34 +59,26 @@ class AcceptanceTest extends TestCase {
 
     /** @test */
     public function someProductsInTexas() {
-        $retailCalculator = new RetailCalculator();
-
-        $actual = $retailCalculator->totalFor([
+        $actual = $this->retailCalculator->totalFor([
             new PurchaseItem(483.15),
             new PurchaseItem(130.90),
             new PurchaseItem(40.70)
         ], 'TX');
 
-        // 695.671875
         $this->assertEquals(695.68, $actual);
     }
 
     /** @test */
     public function singleProductInAlabama() {
-        $retailCalculator = new RetailCalculator();
-
-        $actual = $retailCalculator->totalFor([new PurchaseItem(442.90)], 'AL');
-
+        $actual = $this->retailCalculator->totalFor([new PurchaseItem(442.90)], 'AL');
         $this->assertEquals(460.62, $actual);
     }
 
     /** @test */
     public function singleProductInCalifornia() {
-        $retailCalculator = new RetailCalculator();
-
-        $actual = $retailCalculator->totalFor([new PurchaseItem(685.24)], 'CA');
-
+        $actual = $this->retailCalculator->totalFor([new PurchaseItem(685.24)], 'CA');
         $this->assertEquals(741.78, $actual);
     }
 
 }
+
